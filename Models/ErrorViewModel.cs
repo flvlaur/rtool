f3011016a077641c63cc910188a094d4195872f3
Laurențiu-Flavius Istrate <laurentiu-flavius.istrate@students.plymouth.ac.uk>
2023-07-28 19:57:09 +0100

    RetentionTool v2.0.1
    2.0.1 (Pink)

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RetentionTool___Naplex_MVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
